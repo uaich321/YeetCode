493db344eeb70f5e3bbb6e11cbe1f47c70844f88
uaich321 <code.urjit@protonmail.com>
2026-03-27 16:53:18 -0400

    Update: Valid Anagram

---
+++ String/ValidAnagram.java
@@ -0,0 +1,43 @@
+/*
+  First, we check if the lengths of the two strings are different. 
+  If they are, we can immediately return false since they cannot be anagrams.
+
+  There are two cases to watch out for after this:
+    1. There is a letter in t not in s. Then, we return false.
+    2. There is a repeated letter in t that is not repeated in s. 
+    Then, we return false.
+
+
+    The best way to solve this in O(n) is to use a frequency array that records
+    the frequency of each letter. If the strings are truly anagrams, then each
+    letter should have the same frequency in both strings.
+
+    But, how can we write this in code?
+
+    A good idea would be to add 1 to the array each time we see a letter in s,
+    and subtract 1 from the array each time we see a letter in t. 
+    If the strings are anagrams, then the frequency array should be all 0s at the end, 
+    since we add and substract 1 the same number of times.
+
+    Finally, we loop through the frequency array and check if there are any non-zero values.
+    If there are, then we return false, otherwise we return true.
+ 
+ */
+public class ValidAnagram {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] count = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+        for (int c : count) {
+            if (c != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
