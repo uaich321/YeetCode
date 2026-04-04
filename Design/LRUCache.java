a58632343784aae635d2843d14a2481b2a93bd07
uaich321 <code.urjit@protonmail.com>
2026-04-04 17:11:15 -0600

    LRUCache java design problem

---
+++ Design/LRUCache.java
@@ -0,0 +1,70 @@
+package Design;
+
+import java.util.HashMap;
+
+import org.w3c.dom.Node;
+
+public class LRUCache {
+    class Node {
+        int key;
+        int value;
+        Node prev;
+        Node next;
+        Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+    int capacity;
+    Node head;
+    Node tail;
+    public LRUCache(int capacity) {
+        if (capacity > 0) {
+            this.capacity = capacity;
+        }
+        head = new Node(-1, -1);
+        tail = new Node(-1, -1);
+        head.next = tail;
+        tail.prev = head;
+    }
+    private void removeNode(Node node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+        node.prev = null;
+        node.next = null;
+    }
+    private void addToHead(Node node) {
+        node.next = head.next;
+        node.prev = head;
+        head.next.prev = node;
+        head.next = node;
+    }
+    HashMap<Integer, Node> map = new HashMap<>();
+    public int get(int key) {
+        if (!map.containsKey(key)) {
+            return -1;
+        }
+        Node node = map.get(key);
+        removeNode(node);  
+        addToHead(node);    
+        return node.value;
+    }
+    public void put(int key, int value) {
+        if (map.containsKey(key)) {
+            Node node = map.get(key);
+            node.value = value;
+            removeNode(node);
+            addToHead(node);
+        } else {
+            Node node = new Node(key, value);
+            addToHead(node);
+            map.put(key, node);
+
+            if (map.size() > capacity) {
+                Node lru = tail.prev;
+                removeNode(lru);
+                map.remove(lru.key);
+            }
+        }
+    }
+}
