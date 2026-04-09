8544b2bf40ce1e4f27b1515231ea27f6be837e95
uaich321 <code.urjit@protonmail.com>
2026-04-09 00:21:34 -0400

    Description for ShuffleArray

--- Array/Easy/ShuffleTheArray.java
+++ Array/Easy/ShuffleTheArray.java
@@ -1,5 +1,11 @@
 package Array.Easy;
+/*
+    In order to solve the problem, we simply see that the x's are in even indices and y's
+    are in the odd indices. Recall the formal definition of an even and odd number and use
+    that to assign the indices of the new array to the original array.
 
+    To get the y elements, add n to the current index as they are at index n or above. 
+ */
 public class ShuffleTheArray {
     public int[] shuffle(int[] nums, int n) {
         int[] res = new int[2 * n];
